e62535c622ac64ee7fee92a321bf92f12c87caf9
JAKOB198 <jakob.meh07@gmail.com>
2025-11-10 16:35:45 +0100

    vercel deploy

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -6,18 +6,15 @@ export async function POST(req: Request) {
   const email = body.email;
   const password = body.password;
 
-  // če ni podatkov, ustavi
   if (!email || !password) {
     return NextResponse.json({ message: "Vnesi email in geslo." });
   }
 
-  // preveri, če uporabnik že obstaja
   const user = await prisma.user.findUnique({ where: { email } });
   if (user) {
     return NextResponse.json({ message: "Uporabnik že obstaja." });
   }
 
-  // ustvari novega uporabnika (brez bcrypt, da bo preprosto)
   await prisma.user.create({
     data: { email, password },
   });
